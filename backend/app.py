ded86f66f3ce60e7b43b43a5ed8ba0be4073b481
quikrob00 <Arnettpropertiesllc3@gmail.com>
2025-03-17 11:01:20 -0500

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return 'Backend Live'
+    return "✅ Deal Offer Pro is live and running!"
 
 if __name__ == '__main__':
     app.run(debug=True)
